857735a8612efa3d74ad69f7da624eefce73fb60
Arek.Waliszewski <arkadiusz.waliszewski@forcom.com.pl>
2019-01-30 16:37:30 +0100

    Fix for mono

--- SpringExpressions/Expressions/Expression.cs
+++ SpringExpressions/Expressions/Expression.cs
@@ -101,6 +101,9 @@ namespace SpringExpressions
                 {
                     if (BASENODE_TYPE.IsAssignableFrom(type))
                     {
+                        if (type.IsAbstract || type.IsInterface)
+                            continue;
+
                         ConstructorInfo ctor = type.GetConstructor(new Type[0]);
                         if (ctor != null)
                         {
