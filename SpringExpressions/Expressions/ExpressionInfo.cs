7685df82b24d50248bb9057aec7acccc0ff1c740
Arek.Waliszewski <arkadiusz.waliszewski@forcom.com.pl>
2024-03-04 13:08:50 +0100

    ExpressionInfo - currently for debugging purposes only.

---
+++ SpringExpressions/Expressions/ExpressionInfo.cs
@@ -0,0 +1,38 @@
+﻿using SpringExpressions.Expressions.Compiling.Expressions;
+using System.IO;
+
+namespace SpringExpressions.Expressions
+{
+    public static class ExpressionInfo
+    {
+        public static string DescribeAsXmlString(IStronglyTypedExpression expression)
+        {
+            if (expression is BaseStronglyTypedExpression stronglyTypedExpression)
+            {
+                var springAst = stronglyTypedExpression.ExpressionNode;
+
+                using (TextWriter tw = new StringWriter())
+                {
+                    springAst.xmlSerialize(tw);
+
+                    return tw.ToString();
+                }
+            }
+
+            return "error!";
+        }
+
+
+        public static string DescribeAsStringTree(IStronglyTypedExpression expression)
+        {
+            if (expression is BaseStronglyTypedExpression stronglyTypedExpression)
+            {
+                var springAst = stronglyTypedExpression.ExpressionNode;
+                return springAst.ToTree();
+            }
+
+            return "error!";
+        }
+
+    }
+}
